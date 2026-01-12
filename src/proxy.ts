9321b96ce99d1dbdc013d99814ac70e7f0d7695b
Marcus Gajozo <marcusgajozo@gmail.com>
2026-01-12 12:47:04 -0400

    feat(proxy): config proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,1 +1,22 @@
-export default function Proxy() {}
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isPublicRoute =
+    req.nextUrl.pathname === "/sign-in" || req.nextUrl.pathname === "/sign-in";
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  if (isLoggedIn && isPublicRoute) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
